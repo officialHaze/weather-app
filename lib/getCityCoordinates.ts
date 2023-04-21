f9d1ae9b3ad03ae05ff0ef5a8d493355bcae82d1
Moinak <moinak.dey8@gmail.com>
2023-04-22 03:08:22 +0530

    changed http to https

--- lib/getCityCoordinates.ts
+++ lib/getCityCoordinates.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const getCityCoordinates = async (cityName: string) => {
 	const { data } = await axios.get(
-		`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=73652c4b6d8d79969afcc8c1e0f43482`,
+		`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=73652c4b6d8d79969afcc8c1e0f43482`,
 	);
 	const serialized_data = cityDetailsSerializer(data[0]);
 	return serialized_data;
